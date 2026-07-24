6008b94997f585d8cfb9ef3f045b0e953f99b2a8
sebiimaks <github.sadness446@passmail.net>
2026-07-24 17:28:33 +1000

    Refine Current Hub folder list

--- src/app/pipes/folder-arrows.pipe.ts
+++ src/app/pipes/folder-arrows.pipe.ts
@@ -11,11 +11,23 @@ export class FolderArrowsPipe implements PipeTransform {
    * Return HTML string with `>` arrow instead of the `/` path dividier
    * @param folderPath
    */
-  transform(folderPath: string): string {
+  transform(folderPath: string, markFinalSegment = false): string {
 
     const arrowString = '<span class="icon icon-arrow"></span>';
 
     const htmlString = folderPath.replace(/\/|\\/g, arrowString);
+
+    if (markFinalSegment) {
+      const lastForwardSlash = folderPath.lastIndexOf('/');
+      const lastBackslash = folderPath.lastIndexOf('\\');
+      const lastDivider = Math.max(lastForwardSlash, lastBackslash);
+      const finalSegment = folderPath.substring(lastDivider + 1);
+
+      if (finalSegment) {
+        const parentPath = folderPath.substring(0, lastDivider + 1).replace(/\/|\\/g, arrowString);
+        return `${parentPath}<span class="folder-path-final-segment">${finalSegment}</span>`;
+      }
+    }
 
     return `${htmlString}`;
 
